9add3eb92f1561c134be7f5bc33950ddfbb66e11
Jelle Max <jellemax08@hotmail.com>
2021-04-13 23:07:42 +0200

    temp upload attempt of debounce

--- assets/js/api.js
+++ assets/js/api.js
@@ -9,3 +9,30 @@ async function getRedditPosts(subreddit, sort = undefined) {
 	const rs = await response.json();
 	return await rs.data.children;
 }
+
+const searchSubReddit = debounce(function(){
+		let name = document.querySelector("input").value;
+		if (name.length > 3) {
+			console.log(name);
+			let url = `https://www.reddit.com/api/search_reddit_names.json?query=${name}`;
+			const response = await fetch(url);
+			const rs = await response.json();
+			console.log(rs);
+			}
+		}, 500);
+
+function debounce(func, wait, immediate) {
+	let timeout;
+	return function () {
+		let context = this,
+			args = arguments;
+		let later = function () {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		let callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+}
--- assets/js/index.js
+++ assets/js/index.js
@@ -3,20 +3,29 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
 	fillRedditPosts("ProgrammerHumor", "hot");
+	/* document.querySelector("form").addEventListener("submit", getSubReddit); */
+	document.querySelector("input").addEventListener("input", searchSubReddit);
+}
+
+function getSubReddit(ev) {
+	ev.preventDefault();
+	let value = document.querySelector("input").value;
+	document.querySelector("input").value = "";
+	fillRedditPosts(value);
 }
 
 async function fillRedditPosts(subreddit, sort) {
 	let subRedditContainer = document.querySelector("#redditPosts");
+	subRedditContainer.innerHTML = "";
 	const posts = await getRedditPosts(subreddit);
 	for (let post of posts) {
 		if (post.data.url.indexOf(".jpg") >= 0) {
 			let postLayout = `<article>
 			<h1>${post.data.title}</h1>
-			<img src="${post.data.url}">
+			<figure></figure><img src="${post.data.url}"></figure>
 			</article>`;
 
 			subRedditContainer.innerHTML += postLayout;
-			console.log(post);
 		}
 	}
 }
