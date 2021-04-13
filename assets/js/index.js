f929a89e84b990e9ce78d08834e98efbac0a1f34
master <jellemax08@hotmail.com>
2021-04-14 00:02:44 +0200

    creating searchbox

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,11 +3,16 @@ document.addEventListener("DOMContentLoaded", init);
 
 function init() {
 	fillRedditPosts("aww");
-	document.querySelector("form").addEventListener("submit", getSubReddit);
+	document.querySelector("form").addEventListener("submit", (ev) => {
+		ev.preventDefault();
+		let reddit = document.querySelector("input").value;
+		fillRedditPosts(reddit, "hot");
+	});
 }
 
 async function fillRedditPosts(subreddit, sort) {
 	let subRedditContainer = document.querySelector("#redditPosts");
+	subRedditContainer.innerHTML = "";
 	const posts = await getRedditPosts(subreddit);
 	for (let post of posts) {
 		if (post.data.url.indexOf(".jpg") >= 0) {
