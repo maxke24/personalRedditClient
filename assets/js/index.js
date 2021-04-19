5d6299599ae78cfd14d4db30c61394644b7b896f
jelle <jellemax08@hotmail.com>
2021-04-19 07:45:40 +0200

    adding service worker

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,25 @@
 "use strict";
 document.addEventListener("DOMContentLoaded", init);
 
-function init() {}
+function init() {
+	registerServiceWorker();
+}
 
 window.onscroll = function (ev) {
 	if (window.innerHeight + window.scrollY >= document.body.scrollHeight) {
 		loadExtraRedditPosts();
 	}
 };
+
+function registerServiceWorker() {
+	if ("serviceWorker" in navigator) {
+		navigator.serviceWorker
+			.register("/sw.js")
+			.then((res) =>
+				console.log(
+					`successfully registered serviceWorker with scope ${res.scope}`
+				)
+			)
+			.catch((err) => console.error("Error installing serviceWorker", err));
+	}
+}
